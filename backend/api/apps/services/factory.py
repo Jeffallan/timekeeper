ccd0cdcb590a6077c45ea1ce4340222d7b32b016
Jeff <dshostakovich30@gmail.com>
2021-12-03 10:57:04 -0600

    adds test data for services

--- backend/api/apps/services/factory.py
+++ backend/api/apps/services/factory.py
@@ -5,6 +5,21 @@ from factory.django import DjangoModelFactory
 from datetime import datetime
 import random
 
+from api.apps.users.factory import active_choice
+from api.apps.core.util.services import ServiceChoices
 
-class Service(DjangoModelFactory):
-    pass
+
+class ServiceFactory(DjangoModelFactory):
+    class Meta:
+        model = Service
+    name = factory.Faker("job")
+    service_unit = random.choice([x.name for x in ServiceChoices])
+    is_duration =  factory.LazyFunction(active_choice)
+
+    @factory.post_generation
+    def approved_providers(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for provider in extracted:
+                self.approved_providers.add(provider)
---
+++ backend/api/apps/services/management/commands/gen_services.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from django.db import models, transaction
+from typing import Any, Optional
+import random
+
+
+from api.apps.users.models import User
+from api.apps.services.models import Service
+from api.apps.services.factory import ServiceFactory
+
+SERVICE = 5
+
+def gen_providers():
+    lst = User.objects.filter(is_active=True).values_list("id", flat=True)
+    return random.choices(lst, k=random.randint(3,7))
+
+
+class Command(BaseCommand):
+    help = "Generates test data for services and providers."
+    models = [Service,]
+
+    @transaction.atomic
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        self.stdout.write("Creating test data.")
+        self.stdout.write("Creating services.")
+
+        Service.objects.all().delete()
+
+        self.stdout.write("Creating locations.")
+        for _ in range(SERVICE):
+            sv = ServiceFactory.create(approved_providers=gen_providers())
